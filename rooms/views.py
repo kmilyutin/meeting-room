6b8feabf512deb4b9837a428800a4a86ce41f259
kmilyutin <milyutin.nikolay10@mail.ru>
2026-07-12 06:43:06 +0300

    make DRY with databases

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from rooms.models import Room
+
 def index(request):
     context = {
         'title': 'Поиск переговорной - Booked!',
+        'rooms': Room.objects.all(),
     }
     return render(request, 'rooms/index.html', context)
+
